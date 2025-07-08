be0754d1aa1f956c50bcd0a0ae4834a2125fd99c
emre-mentes <emreowen57@gmail.com>
2025-07-08 09:38:01 +0200

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,4 +1,7 @@
 public class AnotherClass {
     //techpro branc version t7
     public String string1 = "biri sen, biri ben, iki damla yas akti gözlerimden";
+
+    //techpro branc version t8
+    public String string2 =" parildayip duran insanlara bak";
 }
